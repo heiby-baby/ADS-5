096f0fb7b1dc6b978d9e71e98b27b8f718cdcdca
Кандрушин Максим 22-IST-1 <125025831+heiby-baby@users.noreply.github.com>
2023-04-01 02:36:33 +0300

    TSTACK #1

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,38 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+  T* arr;
+  int top;
+public:
+  Tstack() { top = -1; arr = new T[size]; }
+  void push(T value) {
+    if (isFull()) {
+      throw std::string("string is full");
+    } else {
+      arr[++top] = value;
+    }
+  }
+  bool isFull() const {
+    return top == size - 1;
+  }
+  bool isEmpty() const {
+    return top == -1;
+  }
+  const T& Get() {
+    if (isEmpty()) {
+      throw std::string("string is empty");
+    } else {
+      return arr[top];
+    }
+  }
+  const T& Pop() {
+    if (isEmpty()) {
+      throw std::string("string is empty");
+    } else {
+      return arr[top--];
+    }
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
