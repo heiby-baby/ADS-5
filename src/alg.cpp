fed5eb14f3552d2b708e823d829504be9a0b8945
Кандрушин Максим 22-IST-1 <125025831+heiby-baby@users.noreply.github.com>
2023-04-01 04:06:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,106 @@
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+int alphabet(char c) {
+	switch (c) {
+    case('1'): return 1;
+    case('2'): return 2;
+    case('3'): return 3;
+    case('4'): return 4;
+    case('5'): return 5;
+    case('6'): return 6;
+    case('7'): return 7;
+    case('8'): return 8;
+    case('9'): return 9;
+    case('0'): return 0;
+  }
+  return -1; 
 }
 
-int eval(std::string pref) {
-  // добавьте код
+bool Cheching_for_a_Num(char a) {
+  return (a >= '0' && a <= '9');
+}
+
+int get_operator_priority(char op) {
+  if (op == '+' || op == '-') {
+    return 1;
+  }
+  else if (op == '*' || op == '/') {
+    return 2;
+  } else {
+    return 0;
+  }
+}
+
+int ifOperator(char c) {
+  switch (c) {
+    case('+'): return 1;
+    case('-'): return 2;
+    case('*'): return 3;
+    case('/'): return 4;
+  }
   return 0;
+}
+
+std::string infix_to_postfix(std::string inf) {
+  TStack<char,100> s;
+  std::string result = "";
+  for (int i = 0; i < inf.length(); i++) {
+    char c = inf[i];
+    if (Cheching_for_a_Num(c)) {
+      result += c;
+    }
+    else if (c == '(') {
+      s.push(c);
+    }
+    else if (c == ')') {
+      while (!s.isEmpty() && s.Get() != '(') {
+        result += s.Get();
+        s.Pop();
+      }
+      s.Pop();
+    }
+    else if (ifOperator(c)) {
+      while (!s.isEmpty() && get_operator_priority(s.Get()) >= get_operator_priority(c)) {
+        result += s.Get();
+        s.Pop();
+      }
+      s.push(c);
+    }
+  }
+  while (!s.isEmpty()) {
+    result += s.Get();
+    s.Pop();
+  }
+  return result;
+}
+
+int eval(std::string post){
+  TStack<int, 100> s;
+  for (int i = 0; i < post.length(); i++) {
+    char c = post[i];
+    if (Cheching_for_a_Num(c)) {
+      s.push(alphabet(c));
+    } else {
+      int temp1 = s.Get();
+      s.Pop();
+      int temp2 = s.Get();
+      s.Pop();
+      switch (c) {
+        case '+':
+          s.push(temp1 + temp2);
+          break;
+        case '-':
+          s.push(temp1 - temp2);
+          break;
+        case '*':
+          s.push(temp1 * temp2);
+          break;
+        case '/':
+          s.push(temp1 / temp2);
+          break;
+      }
+    }
+  }
+  return s.Get();
 }
